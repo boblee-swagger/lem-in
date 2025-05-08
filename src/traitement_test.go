1c0a229182346803e3faa74aaa9c35c16dc42f1d
kallgueye <gueyekallamoussa@gmail.com>
2025-05-08 18:16:15 +0000

    table driven tests

--- src/traitement_test.go
+++ src/traitement_test.go
@@ -1,43 +1,82 @@
 package src
 
 import (
-	//"fmt"
-	"fmt"
 	"testing"
 )
 
 func TestFileContent(t *testing.T) {
-	filename := "../examples/example00"
-	_, err := FileContent(filename)
-	if err != nil {
-		t.Errorf(`FileContent(filename) = %s`, err)
+	var tests = []struct {
+		name string
+		filename string
+		wantErr  bool
+	}{
+		{"file0exists", "../examples/example00", false},
+		{"file11notExists", "../examples/example011", true},
 	}
-	
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T){
+			if _, err := FileContent(tt.filename); (err != nil) != tt.wantErr{
+				t.Errorf("FileContent(%s) = %s", tt.filename, err)
+			}
+		})
+	}
 }
 
 func TestValidateAntfarmData(t *testing.T) {
-	filename := "../examples/example01"
-	fileLines, _ := FileContent(filename)
-	_, err := ValidateAntfarmData(fileLines)
-	if err != nil {
-		t.Errorf(`ValidateAnteFarmData(fileLines) = %v %v`, fileLines, err)
+	var tests = []struct{
+		name string
+		filename string
+		wantErr bool
+	}{
+		{"normal", "../examples/example01", false},
+		{"confuse", "../examples/example03", false},
+		{"weird", "../examples/example09", true},
 	}
-	fmt.Println("test passed")
+	for _, tt := range tests{
+		t.Run(tt.name, func(t *testing.T){
+			filelines, _ := FileContent(tt.filename)
+			if _, err := ValidateAntfarmData(filelines); (err != nil) != tt.wantErr {
+				t.Errorf("ValidateAntfarmData(%s) = %s", tt.filename, err)
+			}
+		})
+	}
 }
 
-func TestValideLinkFormat(t *testing.T) {
-	link := []string{"hello", "world"}
-
-	if _, err := FormatLink(link); err != nil {
-		t.Errorf("FormatLink(line) = %v %v", link, err)
+func TestFormatLink(t *testing.T) {
+	var tests = []struct{
+		name string
+		fromTo []string
+		wantErr bool
+	}{
+		{"correct", []string{"hello", "world"}, false},
+		{"cyclic link", []string{"no", "no"}, true},
 	}
-	fmt.Println("test passed")
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T){
+			if _,err := FormatLink(tt.fromTo); (err != nil) != tt.wantErr{
+				t.Errorf("ForamtLink(%s) = %s", tt.fromTo, err)
+			}
+		})
+	}
 }
 
 func TestValideRoomFormat(t *testing.T) {
-	room := []string{"c", "1", "2"}
-	if _, err := FormatRoom(room); err != nil {
-		t.Errorf("FormatRoom(rooms[i]) = %v, %v", room, err)
+	var tests = []struct{
+		name string
+		room []string
+		wantErr bool
+	}{
+		{"correct", []string{"numORdigit", "2", "4"}, false},
+		{"bad coordinate", []string{"mustBeInt", "t", "9"}, true},
+		{"not start with L", []string{"Last", "3", "7"}, true},
 	}
-	fmt.Println("test passed")
+	for _,tt := range tests{
+		t.Run(tt.name, func(t *testing.T){
+			if _, err := FormatRoom(tt.room); (err != nil) != tt.wantErr{
+				t.Errorf("FormatRoom(%s) = %s", tt.room, err)
+			}	
+		})
+	}
 }
