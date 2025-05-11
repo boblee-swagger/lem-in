0fdcb730326b95120ffbee65c527333ac2f53e41
kallgueye <gueyekallamoussa@gmail.com>
2025-05-11 12:58:41 +0000

    opp implemented

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"lem-in/src"
 	"log"
 	"os"
+	"fmt"
+
 )
 
 func main() {
+	Start()
+}
+
+func Start() {
 	filename := os.Args[1]
 	fileLines, err := src.FileContent(filename)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	data, err := src.ValidateAntfarmData(fileLines)
+	antFarm, err := src.ValidateAntfarmData(fileLines)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fmt.Printf("number of ants: %d\n",data.NumberOfAnts)
-	fmt.Printf("starting room: %s\n",data.StartingRoom.Name)
-	fmt.Printf("ending room: %s\n",data.EndingRoom.Name)
-	fmt.Printf("rooms: %v\n",data.Rooms)
-	fmt.Printf("links: %v\n",data.Links)
+	fmt.Println(antFarm.Rooms)
+	fmt.Println(antFarm.Links)
+
+	for _, room := range antFarm.GetRooms() {
+		room.SetNeighbors(antFarm)
+	}
 
 	
 
